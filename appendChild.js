3f3ddbb08d140ceab1d4fd942e30deb512818c3c
moonhassan123 <moonhassan9899@gmail.com>
2023-10-02 23:13:24 +0600

    all complet

---
+++ appendChild.js
@@ -0,0 +1,12 @@
+// console.log('connect');
+const placeContainer = document.getElementById('ul-container')
+
+const LI1 = document.createElement('li')
+LI1.innerText = 'sajek';
+
+
+const LI2 = document.createElement('li')
+LI2.innerText = 'dinajpur';
+
+placeContainer.appendChild(LI1)
+placeContainer.appendChild(LI2)
---
+++ dom.js
@@ -0,0 +1,48 @@
+// const student ={
+//     name: 'mehedi',
+//     Age: 23,
+//     id:13,
+//     address:{permantAddress:'dinajpur',presentAddress:'dhaka'}
+// }
+// student.name
+
+// const  byCLASSName =  document.grtElementsByClassName('title')
+// // console.log(byCLASSName);
+
+// for(const tag of byCLASSName){
+//    const useSTyle = tag.style.color = 'red' 
+// //  console.log();
+// }
+
+
+// const names =['mehedi', 'hasan', 'moon']
+
+
+// for (let i=0;i<names.length;i++){
+//     console.log(names[i]);
+// }
+// const name = 'hasan'
+// name ='mehedi'
+// console.log(name);
+
+
+
+// for (const name of names){
+//     console.log(name + ' ' + 'shaha');
+// }
+
+
+
+document.grtElementsById('list').style.color ='red'
+const h1Collection = document. querySelectorAll('h1')
+console.log(h1Collection);
+for ( const h1 of h1Collection){
+    // console.log(h1);
+    // h1.style.backgroundColor='grey'
+    h1.innerText='no titel availbe'
+}
+
+const container = document.getElementById('list').
+container.innerText='i got changed';
+
+
